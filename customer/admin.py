9a74e38615f3980c433f61b6d8810de147b7f011
BradleyMurrell <b.murrell89@gmail.com>
2022-06-26 14:34:59 +0000

    Link burger, side and drink pages to the order page

--- customer/admin.py
+++ customer/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from .models import Burger, Side, Drink
+
+class BurgerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+admin.site.register(Burger, BurgerAdmin)
+
+class SideAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+admin.site.register(Side, SideAdmin)
+
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+admin.site.register(Drink, DrinkAdmin)
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Burger, Side, Drink
 
 def index(request):
     return render(request, 'customer/index.html')
